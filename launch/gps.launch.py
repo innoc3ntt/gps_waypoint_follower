e85459fabef4384acc22643ef83dd9db725e30ae
tim <timothy.tan.93@gmail.com>
2022-11-07 09:21:42 +0800

    edit launch files

--- launch/gps.launch.py
+++ launch/gps.launch.py
@@ -30,14 +30,14 @@ def generate_launch_description():
 
     pkg_share = get_package_share_directory("gps_waypoint_follower")
 
-    params = os.path.join(pkg_share, "params/ekf_gps_2.yaml")
+    params = os.path.join(pkg_share, "params/ekf_gps_3.yaml")
 
     lifecycle_nodes = ["gps_waypoint_follower"]
 
     # * Declares
     declare_use_sim_time_cmd = DeclareLaunchArgument(
         name="use_sim_time",
-        default_value="True",
+        default_value="False",
         description="Use simulation (Gazebo) clock if true",
     )
 
@@ -55,19 +55,6 @@ def generate_launch_description():
         namespace="",
         output="screen",
     )
-
-    # start_map_server_cmd = LifecycleNode(
-    #     package="nav2_map_server",
-    #     executable="map_server",
-    #     name="map_server",
-    #     namespace="",
-    #     output="screen",
-    #     parameters=[
-    #         {"use_sim_time": use_sim_time},
-    #         {"autostart": autostart},
-    #         {"yaml_filename": map_yaml_file},
-    #     ],
-    # )
 
     gps_lifecycle = Node(
         package="nav2_lifecycle_manager",
@@ -90,9 +77,9 @@ def generate_launch_description():
         output="screen",
         parameters=[params, {"use_sim_time": use_sim_time}],
         remappings=[
-            ("imu/data", "imu/data"),
-            ("gps/fix", "gps/fix"),  # !!! for demo
-            # ("gps/fix", "imu/nav_sat_fix"),
+            ("imu", "imu/data"),
+            # ("gps/fix", "gps/fix"),  # !!! for demo
+            ("gps/fix", "imu/nav_sat_fix"),
             ("gps/filtered", "gps/filtered"),
             ("odometry/gps", "odometry/gps"),
             ("odometry/filtered", "odometry/global"),
@@ -132,7 +119,6 @@ def generate_launch_description():
     ld.add_action(declare_autostart_cmd)
     ld.add_action(declare_use_sim_time_cmd)
 
-    # ld.add_action(start_map_server_cmd)
     ld.add_action(driver_node)
     ld.add_action(gps_lifecycle)
     ld.add_action(start_navsat_transform_cmd)
---
+++ launch/nav2.launch.py
@@ -0,0 +1,139 @@
+# Author: Addison Sears-Collins
+# Date: September 2, 2021
+# Description: Launch a basic mobile robot using the ROS 2 Navigation Stack
+# https://automaticaddison.com
+
+import os
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition, UnlessCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, LaunchConfiguration, PythonExpression
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+
+    # Set the path to different files and folders.
+    nav2_dir = get_package_share_directory("nav2_bringup")
+    pkg_share = get_package_share_directory("gps_waypoint_follower")
+
+    default_rviz_config_path = os.path.join(pkg_share, "rviz/nav2_config.rviz")
+
+    nav2_launch_dir = os.path.join(nav2_dir, "launch")
+    # static_map_path = os.path.join(pkg_share, "maps", "test.yaml")
+    static_map_path = os.path.join(pkg_share, "maps", "ezoneguild.yaml")
+    nav2_params_path = os.path.join(pkg_share, "params", "nav2_params.yaml")
+
+    # Launch configuration variables specific to simulation
+    autostart = LaunchConfiguration("autostart")
+    map_yaml_file = LaunchConfiguration("map")
+    namespace = LaunchConfiguration("namespace")
+    params_file = LaunchConfiguration("params_file")
+    rviz_config_file = LaunchConfiguration("rviz_config_file")
+    slam = LaunchConfiguration("slam")
+    use_namespace = LaunchConfiguration("use_namespace")
+    use_rviz = LaunchConfiguration("use_rviz")
+    use_sim_time = LaunchConfiguration("use_sim_time")
+
+    # Map fully qualified names to relative ones so the node's namespace can be prepended.
+    # In case of the transforms (tf), currently, there doesn't seem to be a better alternative
+    # https://github.com/ros/geometry2/issues/32
+    # https://github.com/ros/robot_state_publisher/pull/30
+    # TODO(orduno) Substitute with `PushNodeRemapping`
+    #              https://github.com/ros2/launch_ros/issues/56
+    remappings = [("/tf", "tf"), ("/tf_static", "tf_static")]
+
+    # Declare the launch arguments
+    declare_namespace_cmd = DeclareLaunchArgument(
+        name="namespace", default_value="", description="Top-level namespace"
+    )
+
+    declare_use_namespace_cmd = DeclareLaunchArgument(
+        name="use_namespace",
+        default_value="false",
+        description="Whether to apply a namespace to the navigation stack",
+    )
+
+    declare_autostart_cmd = DeclareLaunchArgument(
+        name="autostart",
+        default_value="true",
+        description="Automatically startup the nav2 stack",
+    )
+
+    declare_map_yaml_cmd = DeclareLaunchArgument(
+        name="map",
+        default_value=static_map_path,
+        description="Full path to map file to load",
+    )
+
+    declare_params_file_cmd = DeclareLaunchArgument(
+        name="params_file",
+        default_value=nav2_params_path,
+        description="Full path to the ROS2 parameters file to use for all launched nodes",
+    )
+
+    declare_rviz_config_file_cmd = DeclareLaunchArgument(
+        name="rviz_config_file",
+        default_value=default_rviz_config_path,
+        description="Full path to the RVIZ config file to use",
+    )
+
+    declare_slam_cmd = DeclareLaunchArgument(
+        name="slam", default_value="False", description="Whether to run SLAM"
+    )
+
+    declare_use_rviz_cmd = DeclareLaunchArgument(
+        name="use_rviz", default_value="True", description="Whether to start RVIZ"
+    )
+
+    declare_use_sim_time_cmd = DeclareLaunchArgument(
+        name="use_sim_time",
+        default_value="false",
+        description="Use simulation (Gazebo) clock if true",
+    )
+
+    # Launch RViz
+    start_rviz_cmd = Node(
+        condition=IfCondition(use_rviz),
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen",
+        arguments=["-d", rviz_config_file],
+    )
+
+    start_ros2_navigation_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(nav2_launch_dir, "bringup_launch.py")
+        ),
+        launch_arguments={
+            "namespace": namespace,
+            "use_namespace": use_namespace,
+            "slam": slam,
+            "map": map_yaml_file,  # ! only valid for localization launch in nav2 bringup
+            "use_sim_time": use_sim_time,
+            "params_file": params_file,
+            "autostart": autostart,
+        }.items(),
+    )
+
+    # Create the launch description and populate
+    ld = LaunchDescription()
+
+    # Declare the launch options
+    ld.add_action(declare_namespace_cmd)
+    ld.add_action(declare_use_namespace_cmd)
+    ld.add_action(declare_autostart_cmd)
+    ld.add_action(declare_map_yaml_cmd)
+    ld.add_action(declare_params_file_cmd)
+    ld.add_action(declare_rviz_config_file_cmd)
+    ld.add_action(declare_slam_cmd)
+    ld.add_action(declare_use_rviz_cmd)
+    ld.add_action(declare_use_sim_time_cmd)
+
+    # Add any actions
+    ld.add_action(start_rviz_cmd)
+    ld.add_action(start_ros2_navigation_cmd)
+    return ld
